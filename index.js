07f7274079e263e45be5d300c10f377ae2ccb377
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-21 21:50:01 +0530

    Add DELETE /parcels/:id endpoint for parcel deletion

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 
@@ -55,6 +55,15 @@ async function run() {
       // parcel created time
       parcel.createdAt = new Date();
       const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
+
+    // to delete
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await parcelsCollection.deleteOne(query);
       res.send(result);
     });
 
