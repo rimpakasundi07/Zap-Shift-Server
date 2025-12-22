7e0728faa5b59fe60d1860f10d8ee2159eb10c5c
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-22 21:31:10 +0530

    Add GET /parcels/:id endpoint for retrieving single parcel by ID

--- index.js
+++ index.js
@@ -48,10 +48,20 @@ async function run() {
       res.send(result);
     });
 
+    // payment
+
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.findOne(query);
+      res.send(result);
+    });
+
     // to add
 
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
+
       // parcel created time
       parcel.createdAt = new Date();
       const result = await parcelsCollection.insertOne(parcel);
