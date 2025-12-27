9f916882e06c02c1c55fab3a8d0f9cc3a7c5f1b1
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-27 21:09:44 +0530

    Remove firebase admin sdk from repo and add to gitignore

--- index.js
+++ index.js
@@ -32,14 +32,18 @@ function generateTrackingId() {
 app.use(express.json());
 app.use(cors());
 
-const verifyFBToken = (req, res, next) => {
-  console.log("headers in the middleware", req.headers.authorization);
+const verifyFBToken = async (req, res, next) => {
+  // console.log("headers in the middleware", req.headers.authorization);
   const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).send({ message: "unauthorized access" });
   }
-
+  try {
+    const idToken = token.split("")[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    console.log("decoded in the token", decoded);
+  } catch (err) {}
   next();
 };
 
